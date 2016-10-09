18e85f8d1e6af15f6b79e1a5679b61a81ab2c91f
Dll-loinver <loinverbaby@gmail.com>
2016-10-09 15:20:40 +0800

    第一次改版

---
+++ unpackage/Dev/js/define/index.js
@@ -0,0 +1,6 @@
+define(function(require, exports, module) {
+  window.addEventListener('reLode', function() {
+    window.location.reload();
+  });
+  var common = require('./common');
+});
---
+++ unpackage/Dev/js/define/setting.js
@@ -0,0 +1,8 @@
+define(function(require, exports, module) {
+  window.addEventListener('reLode', function() {
+    window.location.reload();
+  });
+  var laytpl = require("../lib/laytpl");
+  console.log("加载成功了");
+  
+});
